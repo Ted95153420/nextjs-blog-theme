f433a56709c688d69d6efa3c1bfea575eb0bdc3c
Ted Milne <tedmilne@proton.me>
2025-09-03 13:47:38 -0700

    blog post + changed blog title.

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,7 +4,7 @@ export const getGlobalData = () => {
     : 'Transition into Data Science';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Data Science Journey';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
